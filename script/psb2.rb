d4cafd57289646e12f42502bc5415ae0d765405b
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2017-11-03 14:43:05 +0100

    The parser should now be able to detect players joining game.

--- script/psb2.rb
+++ script/psb2.rb
@@ -1,7 +1,7 @@
 require "net/http"
 
 def contains_relevant_data?(line)
-  line.match(/DBG: Game Turn/) || line.match(/NetTurnComplete/) || line.match(/NetTurnUnready/)
+  line.match(/DBG: Game Turn/) || line.match(/NetTurnComplete/) || line.match(/NetTurnUnready/) || line.match(/NetPlayerReady/)
 end
 
 def send_data(game_name:, value:, entry_type:, timestamp:)
@@ -18,6 +18,8 @@ def send_data(game_name:, value:, entry_type:, timestamp:)
 end
 
 game_name = ARGV[0]
+players_count = ARGV[1]
+
 iterations_counter = 0
 last_line_read = 0
 loop do
@@ -29,15 +31,21 @@ loop do
     if lines_counter >= previous_last_line_read && contains_relevant_data?(line)
       puts line
       parts = line.split
+      timestamp = parts.first.gsub(/(\[|\])/, "")
       if line.match(/DBG: Game Turn/)
-        send_data(game_name: game_name, value: parts.last, entry_type: "NewTurn", timestamp: parts.first.gsub(/(\[|\])/, ""))
+        send_data(game_name: game_name, value: parts.last, entry_type: "NewTurn", timestamp: timestamp)
       elsif line.match(/NetTurnComplete/)
         player_number = parts[8].gsub(",", "")
-        if player_number.match(/\d/)
-          send_data(game_name: game_name, value: player_number, entry_type: "PlayerEndedTurn", timestamp: parts.first.gsub(/(\[|\])/, ""))
+        if player_number.match(/\d/) && player_number.to_i < players_count
+          send_data(game_name: game_name, value: player_number, entry_type: "PlayerEndedTurn", timestamp: timestamp)
         end
       elsif line.match(/NetTurnUnready/)
-        send_data(game_name: game_name, value: parts[8], entry_type: "PlayerEndTurnCancelled", timestamp: parts.first.gsub(/(\[|\])/, ""))
+        send_data(game_name: game_name, value: parts[8], entry_type: "PlayerEndTurnCancelled", timestamp: timestamp)
+      elsif line.match(/NetPlayerReady/)
+        player_number = line.scan(/\d/)[-3]
+        if player_number < players_count
+          send_data(game_name: game_name, value: player_number, entry_type: "PlayerJoined", timestamp: timestamp)
+        end
       end
     end
     lines_counter += 1
