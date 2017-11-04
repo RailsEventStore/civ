c07f9981f8cc8cacb8ec2e28d35ac9f0da95534c
Paweł Pacana <pawel.pacana@gmail.com>
2017-11-04 19:38:34 +0100

    Start with domain events received from dedicated server.

--- game/lib/game.rb
+++ game/lib/game.rb
@@ -1,3 +1,9 @@
-module Game
+require 'domain_event'
 
+module Game
+  PlayerConnected        = Class.new(DomainEvent)
+  PlayerDisconnected     = Class.new(DomainEvent)
+  PlayerEndedTurn        = Class.new(DomainEvent)
+  PlayerEndTurnCancelled = Class.new(DomainEvent)
+  NewTurnStarted         = Class.new(DomainEvent)
 end
