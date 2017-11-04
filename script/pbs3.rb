a2eb5b04c9b47738326175d761cdad100ba94998
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2017-11-04 23:10:20 +0100

    A new version of pbs script using newly extracted logs parser service.

---
+++ script/pbs3.rb
@@ -0,0 +1,43 @@
+require "net/http"
+require_relative "service"
+
+def send_data(game_name:, value:, entry_type:, timestamp:)
+  http = Net::HTTP.new("fierce-reaches-40697.herokuapp.com")
+
+  request = Net::HTTP::Post.new("/pitboss_entries")
+  request.set_form_data({
+    "pitboss_entry[game_name]" => game_name,
+    "pitboss_entry[value]" => value,
+    "pitboss_entry[entry_type]" => entry_type,
+    "pitboss_entry[timestamp]" => timestamp})
+  response = http.request(request)
+  puts response.inspect
+end
+
+game_name = ARGV[0]
+players_count = ARGV[1].to_i
+parser = LogsParser::Service.new(game_name, players_count)
+
+iterations_counter = 0
+last_line_read = 0
+loop do
+  puts "Iteration #{iterations_counter}"
+  puts "Processing log file from line #{last_line_read}..."
+  lines_counter = 0
+  previous_last_line_read = last_line_read
+  IO.foreach("net_message_debug.log") do |line|
+    if result = parser.call(line)
+      send_data(
+        game_name:  result.game_name,
+        value:      result.data,
+        entry_type: result.entry_type,
+        timestamp:  result.timestamp
+      )
+    end
+
+    lines_counter += 1
+    last_line_read = lines_counter
+  end
+  iterations_counter += 1
+  sleep 60
+end
