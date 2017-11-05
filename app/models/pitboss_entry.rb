a752bef53456d32449d34a076d2675baf916fd02
Paweł Pacana <pawel.pacana@gmail.com>
2017-11-05 11:49:38 +0100

    Cover all domain events from logs.

--- app/models/pitboss_entry.rb
+++ app/models/pitboss_entry.rb
@@ -1,5 +1,18 @@
 class PitbossEntry < ApplicationRecord
   after_create_commit do
-    Rails.configuration.event_store.publish_event(Game::NewTurnStarted.new)
+    event_store = Rails.configuration.event_store
+
+    case entry_type
+    when "NewTurnStarted"
+      event_store.publish_event(Game::NewTurnStarted.new)
+    when "PlayerEndedTurn"
+      event_store.publish_event(Game::PlayerEndedTurn.new)
+    when "PlayerEndTurnCancelled"
+      event_store.publish_event(Game::PlayerEndTurnCancelled.new)
+    when "PlayerConnected"
+      event_store.publish_event(Game::PlayerConnected.new)
+    when "PlayerDisconnected"
+      event_store.publish_event(Game::PlayerDisconnected.new)
+    end
   end
 end
--- spec/domain_events_from_entries_spec.rb
+++ spec/domain_events_from_entries_spec.rb
@@ -15,4 +15,48 @@ RSpec.describe "glue entries with domain events" do
 
     expect(event_store).to have_published(an_event(Game::NewTurnStarted))
   end
+
+  specify "PlayerEndedTurn" do
+    PitbossEntry.create(
+      timestamp: 0,
+      value: 4,
+      entry_type: "PlayerEndedTurn",
+      game_name: "dummy"
+    )
+
+    expect(event_store).to have_published(an_event(Game::PlayerEndedTurn))
+  end
+
+  specify "PlayerEndTurnCancelled" do
+    PitbossEntry.create(
+      timestamp: 0,
+      value: 4,
+      entry_type: "PlayerEndTurnCancelled",
+      game_name: "dummy"
+    )
+
+    expect(event_store).to have_published(an_event(Game::PlayerEndTurnCancelled))
+  end
+
+  specify "PlayerConnected" do
+    PitbossEntry.create(
+      timestamp: 0,
+      value: 4,
+      entry_type: "PlayerConnected",
+      game_name: "dummy"
+    )
+
+    expect(event_store).to have_published(an_event(Game::PlayerConnected))
+  end
+
+  specify "PlayerDisconnected" do
+    PitbossEntry.create(
+      timestamp: 0,
+      value: 4,
+      entry_type: "PlayerDisconnected",
+      game_name: "dummy"
+    )
+
+    expect(event_store).to have_published(an_event(Game::PlayerDisconnected))
+  end
 end
