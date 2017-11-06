8fafaf50c4a70b96eb7c1e611a22127fa09004da
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2017-11-06 22:41:11 +0100

    Improved version of the script.
    * it uses http adapter from LogsParser module
    * host where data is to be sent needs to be explicitely passed as an arg

--- script/pbs3.rb
+++ script/pbs3.rb
@@ -1,23 +1,12 @@
 require "net/http"
-require_relative "service"
+require_relative "logs_parser"
 
-def send_data(game_name:, value:, entry_type:, timestamp:)
-  http = Net::HTTP.new("fierce-reaches-40697.herokuapp.com")
-
-  request = Net::HTTP::Post.new("/pitboss_entries")
-  request.set_form_data({
-    "pitboss_entry[game_name]" => game_name,
-    "pitboss_entry[value]" => value,
-    "pitboss_entry[entry_type]" => entry_type,
-    "pitboss_entry[timestamp]" => timestamp})
-  response = http.request(request)
-  puts response.inspect
-end
-
-game_name = ARGV[0]
-players_count = ARGV[1].to_i
-last_line_read = ARGV[2].to_i || 0
-parser = LogsParser::Service.new(game_name, players_count)
+game_name      = ARGV[0]
+players_count  = ARGV[1].to_i
+host           = ARGV[2]
+last_line_read = ARGV[3].to_i || 0
+parser       = LogsParser::Service.new(game_name, players_count)
+http_adapter = LogsParser::HttpAdapter.new(host)
 
 iterations_counter = 0
 loop do
@@ -27,12 +16,8 @@ loop do
   previous_last_line_read = last_line_read
   IO.foreach("net_message_debug.log") do |line|
     if result = parser.call(line)
-      send_data(
-        game_name:  result.game_name,
-        value:      result.data,
-        entry_type: result.entry_type,
-        timestamp:  result.timestamp
-      )
+      response = http_adapter.send_data(result)
+      puts response.inspect
     end
 
     lines_counter += 1
