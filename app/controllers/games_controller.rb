591a9f08ef15bd0354159e5c3fd1466db49c3de4
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2017-11-09 22:40:14 +0100

    Game name is displayed on game status page.
    It's convenient to see game name because we started hosting more than one game.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,12 +3,14 @@ class GamesController < ApplicationController
     current_turn = Game::CurrentTurn.new(Rails.configuration.event_store)
       .call("Game$#{params[:id]}")
     time_left    = current_turn.ends_at - Time.zone.now
+    game_name = ReadModel::Game.find(params[:id]).name.capitalize
 
     render :show,
       locals: {
         turn: current_turn.turn,
         unfinished_players: Player.where(id: current_turn.unfinished_player_ids),
-        time_left: time_left
+        time_left: time_left,
+        game_name: game_name,
       }
   end
 end
