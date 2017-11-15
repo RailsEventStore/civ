e63947d0233b9866c59886fa87d4ca764754180e
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2017-11-15 13:40:16 +0100

    Updated host game script.

--- script/host_new_game_with_new_players.rb
+++ script/host_new_game_with_new_players.rb
@@ -1,21 +1,22 @@
+#host game
 event_store = Rails.configuration.event_store
 service = Game::Service.new(event_store)
 game_id = SecureRandom.uuid
 host_game_command = Game::HostGame.new(game_id, 24.hours)
 service.host_game(host_game_command)
 game_read_model = ReadModel::Game.find(game_id)
-game_read_model.name = "arkency2"
+game_read_model.name = "arkency1"
 game_read_model.save
 
+#create players
 Player.create!(steam_name: "swistak35", slack_name: "swistak")
 Player.create!(steam_name: "jura55", slack_name: "jorgen")
 Player.create!(steam_name: "halkye", slack_name: "halki")
-Player.create!(steam_name: "The Rubyist", slack_name: "pkondzior")
+Player.create!(steam_name: "The Rubyist", slack_name: "samsonmiodek")
 
-[ {slack_name: "swistak", slot_id: 0}, {slack_name: "jorgen", slot_id: 1},
-  {slack_name: "halki", slot_id: 2}, {slack_name: "dysk", slot_id: 3},
-  {slack_name: "pkondzior", slot_id: 7} ].each do |player_data|
-  player = Player.find_by(slack_name: player_data[:slack_name])
-  command = Game::RegisterPlayer.new(game_id, player.id, player_data[:slot_id])
+#register players
+["halkye", "tango_mig", "pan_sarin", "swistak35", "The Rubyist", "jura55", "jamesworthy", "dysk"].each_with_index do |steam_name, index|
+  player = Player.find_by(steam_name: steam_name)
+  command = Game::RegisterPlayer.new(game_id, player.id, index)
   service.register_player(command)
 end
