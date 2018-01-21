989a87b7537983c036bfbe2aed1d8035d851cf75
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2018-01-21 23:26:24 +0100

    Handler subscribed to NewTurnStarted event.

--- config/initializers/rails_event_store.rb
+++ config/initializers/rails_event_store.rb
@@ -1,5 +1,6 @@
 Rails.configuration.to_prepare do
   Rails.configuration.event_store = RailsEventStore::Client.new.tap do |client|
     client.subscribe(->(event) { ReadModel::GameReadModel.handle_game_hosted(event) }, [Game::GameHosted])
+    client.subscribe(->(event) { Notifications::SlackNotifier.new(logger: Rails.logger) }, [Game::NewTurnStarted])
   end
 end
