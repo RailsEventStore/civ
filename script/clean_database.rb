383bd81575fcde842fd8506505f38724c3ee747d
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2018-01-21 22:41:13 +0100

    Improved db clean script.

--- script/clean_database.rb
+++ script/clean_database.rb
@@ -6,7 +6,9 @@ Game::PlayerEndTurnCancelled
 Game::NewTurnStarted
 )
 
-RailsEventStoreActiveRecord::Event.where("created_at < '2017-11-26 00:00:00'").where(event_type: events_to_delete).each do |event|
+RailsEventStoreActiveRecord::Event.where("created_at < ?", Time.current - 2.days).where(event_type: events_to_delete).each do |event|
   RailsEventStoreActiveRecord::EventInStream.where(event_id: event.id).delete_all
   event.delete
 end
+
+PitbossEntry.delete_all
