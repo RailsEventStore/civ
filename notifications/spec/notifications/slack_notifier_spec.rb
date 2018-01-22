4e3d662d729716db89b1fb54f5107d8215f9bfdb
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2018-01-22 10:51:42 +0100

    Added expection to the stub that is should have been requested.

--- notifications/spec/notifications/slack_notifier_spec.rb
+++ notifications/spec/notifications/slack_notifier_spec.rb
@@ -38,7 +38,7 @@ module Notifications
 
     specify do
       event = Game::NewTurnStarted.new(data: { turn: 1, game_id: game_id })
-      stub_request(:post, "https://slack.com/api/chat.postMessage").
+      stub = stub_request(:post, "https://slack.com/api/chat.postMessage").
          with(body: {"as_user"=>"true",
             "channel"=>game_slack_channel,
             "text"=>game_read_model.build_slack_new_turn_message(event.data),
@@ -49,6 +49,7 @@ module Notifications
             'User-Agent'=>'Slack Ruby Client/0.11.0'}).
          to_return(status: 200, body: {ok: true}.to_json, headers: {})
       event_store.publish_event(event, stream_name: game_id)
+      expect(stub).to have_been_requested
     end
   end
 end
