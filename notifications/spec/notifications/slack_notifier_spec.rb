f462e136a2cd06a90c4f5b7e77ecf182ea438470
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2018-01-25 13:35:16 +0100

    Spec expanded to test if the last remaining player is pinged if there is more than one player to be pinged intially.

--- notifications/spec/notifications/slack_notifier_spec.rb
+++ notifications/spec/notifications/slack_notifier_spec.rb
@@ -101,11 +101,24 @@ module Notifications
         number_of_remaining_players_to_notify: 2,
       )
 
-      stub = stub_request(:post, "https://slack.com/api/chat.postMessage").
+      stub0 = stub_request(:post, "https://slack.com/api/chat.postMessage").
          with(body: {"as_user"=>"false",
             "channel"=>"#arkency58",
             "icon_url"=>"https://vignette.wikia.nocookie.net/civilization/images/3/36/Gandhi_%28Civ5%29.png/revision/latest?cb=20121104232443",
             "text"=>"Turn <@slack_user> <@anohter_player>",
+            "token"=>"xoxb-302139800755-nR1O848GLyVS5ZfNNMpBLm0b"},
+          headers: {'Accept'=>'application/json; charset=utf-8',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type'=>'application/x-www-form-urlencoded',
+            'User-Agent'=>'Slack Ruby Client/0.11.0'}).
+         to_return(status: 200, body: {ok: true}.to_json, headers: {})
+
+
+      stub1 = stub_request(:post, "https://slack.com/api/chat.postMessage").
+         with(body: {"as_user"=>"false",
+            "channel"=>"#arkency58",
+            "icon_url"=>"https://vignette.wikia.nocookie.net/civilization/images/3/36/Gandhi_%28Civ5%29.png/revision/latest?cb=20121104232443",
+            "text"=>"Turn <@slack_user>",
             "token"=>"xoxb-302139800755-nR1O848GLyVS5ZfNNMpBLm0b"},
           headers: {'Accept'=>'application/json; charset=utf-8',
             'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
@@ -121,8 +134,11 @@ module Notifications
         Game::NewTurnStarted.new(data: { turn: 1 }),
         Game::PlayerEndedTurn.new(data: { slot: 3, game_id: game_id }),
         Game::PlayerDisconnected.new(data: { slot: 3, game_id: game_id }),
+        Game::PlayerEndedTurn.new(data: { slot: 2, game_id: game_id }),
+        Game::PlayerDisconnected.new(data: { slot: 2, game_id: game_id }),
       )
-      expect(stub).to have_been_requested
+      expect(stub0).to have_been_requested
+      expect(stub1).to have_been_requested
     end
   end
 end
