c28329f1544cf9d319e919d1cb477af5d09b6033
Paweł Pacana <pawel.pacana@gmail.com>
2022-03-24 23:43:19 +0100

    Zeitwerk over old, hacky autoloader

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,7 @@ module PitbossStats
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.autoloader = :zeitwerk
   end
 end
