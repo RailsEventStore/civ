81f8db571e73bcc82a16f8357a60c600e5894656
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2022-07-13 22:33:04 +0200

    Slothfulness factor for each player is calculated base on player stats  and displayed on players list.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,16 @@
 class PlayersController < ApplicationController
+  EnrichedPlayer = Struct.new(:id, :slack_name, :steam_name, :slothfulness)
+
   def index
-    @players = Player.all
+    @players = Player.all.map do |player|
+      stat = ReadModel::PlayerStat.find_by(player_id: player.id)
+      slothfulness = if stat&.turns_taken.to_i != 0
+        (stat&.turns_last.to_i / stat&.turns_taken.to_d).round(2)
+      else
+        0
+      end
+      EnrichedPlayer.new(player.id, player.slack_name, player.steam_name, slothfulness)
+    end.sort_by(&:slothfulness).reverse
   end
 
   def new
