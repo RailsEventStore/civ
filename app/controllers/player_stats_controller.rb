9a84de10c06f7d2d7427c203e86eef6bd4ddc532
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2024-06-18 23:36:34 +0200

    When displaying slothfulness stats for a specific game only players registed for the game are shown.

--- app/controllers/player_stats_controller.rb
+++ app/controllers/player_stats_controller.rb
@@ -3,8 +3,14 @@ class PlayerStatsController < ApplicationController
 
   def index
     game_id = params[:game_id] || "all"
-    @players = Player
-      .all
+    relevant_players = if game_id == "all"
+      Player.all
+    else
+      players = Game::Players.new(Rails.configuration.event_store).call("Game$#{game_id}")
+      Player.where(id: players.player_ids)
+    end
+
+    @players = relevant_players
       .map do |player|
         stat = ReadModel::PlayerStat.find_by(player_id: player.id, game_id: game_id)
         EnrichedPlayer.new(player.id, player.slack_name, player.steam_name, stat&.slothfulness.to_d)
