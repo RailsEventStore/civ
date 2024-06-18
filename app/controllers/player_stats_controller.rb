88170d30776b9186bc8630da02e1138314c9ca36
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2024-06-18 16:49:13 +0200

    Allow to display per game slothfulness stats.

--- app/controllers/player_stats_controller.rb
+++ app/controllers/player_stats_controller.rb
@@ -2,10 +2,11 @@ class PlayerStatsController < ApplicationController
   EnrichedPlayer = Struct.new(:id, :slack_name, :steam_name, :slothfulness)
 
   def index
+    game_id = params[:game_id] || "all"
     @players = Player
       .all
       .map do |player|
-        stat = ReadModel::PlayerStat.find_by(player_id: player.id, game_id: "all")
+        stat = ReadModel::PlayerStat.find_by(player_id: player.id, game_id: game_id)
         EnrichedPlayer.new(player.id, player.slack_name, player.steam_name, stat&.slothfulness.to_d)
       end
       .sort_by(&:slothfulness)
