62940b40bb8a5053723757ae32cf6b6ba90ee002
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2024-07-02 23:41:05 +0200

    The helper is no longer missing.

--- stats/spec/spec_helper.rb
+++ stats/spec/spec_helper.rb
@@ -1,27 +1,27 @@
 ENV["RAILS_ENV"] = "test"
 
-$LOAD_PATH.push File.expand_path("../../../spec", __FILE__)
-$LOAD_PATH.push File.expand_path("../../lib", __FILE__)
-$LOAD_PATH.push File.expand_path("../../../game/lib", __FILE__)
+$LOAD_PATH.push(File.expand_path("../../../spec", __FILE__))
+$LOAD_PATH.push(File.expand_path("../../lib", __FILE__))
+$LOAD_PATH.push(File.expand_path("../../../game/lib", __FILE__))
 
 require "stats"
 require "game"
 require "ruby_event_store/rspec"
+require "rails_helper"
 
 module InMemoryEventStore
   def event_store
-    @event_store ||=
-      RailsEventStore::Client
-        .new(repository: RailsEventStore::InMemoryRepository.new)
-        .tap do |client|
-          client.subscribe(
-            ->(event) { Stats::StatsCollector.new(event_store: client).call(event) },
-            to: [Game::NewTurnStarted, Game::PlayerDisconnected]
-          )
-          client.subscribe(
-            ->(event) { Notifications::SlackNotifier.new(logger: Rails.logger, event_store: client).call(event) },
-            to: [Game::NewTurnStarted, Game::PlayerDisconnected]
-          )
-        end
+    @event_store ||= RailsEventStore::Client
+      .new(repository: RailsEventStore::InMemoryRepository.new)
+      .tap do |client|
+        client.subscribe(
+          -> (event) { Stats::StatsCollector.new(event_store: client).call(event) },
+          to: [Game::NewTurnStarted, Game::PlayerDisconnected]
+        )
+        client.subscribe(
+          -> (event) { Notifications::SlackNotifier.new(logger: Rails.logger, event_store: client).call(event) },
+          to: [Game::NewTurnStarted, Game::PlayerDisconnected]
+        )
+      end
   end
 end
