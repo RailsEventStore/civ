ccf0f9774d94d5e1af0e1f77977a3d1cb2771246
Jakub Rozmiarek <jakub.rozmiarek@gmail.com>
2025-09-13 20:15:54 +0200

    Check if exception causes the spec to fail.

--- notifications/lib/notifications/slack_notifier.rb
+++ notifications/lib/notifications/slack_notifier.rb
@@ -53,7 +53,7 @@ module Notifications
       game = ReadModel::GameReadModel.find_by(id: event.data[:game_id])
       return unless game && game.slack_token
       player_id = game.registered_slots&.dig(event.data[:slot])
-      player = Player.find(player_id) if player_id
+      player = Player.find_by(id: player_id) if player_id
       CustomSlackClient.post_message(
         channel: game.slack_channel,
         text: game.build_slack_timer_reset_message(event.data, player),
